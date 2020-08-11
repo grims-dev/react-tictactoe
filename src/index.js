a832c2cddab3e34da70e45209ea091ab8467753b
Ciaran <ciarangrimshawdev@gmail.com>
2020-08-11 14:10:01 +0100

    Add key value to Square component

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
